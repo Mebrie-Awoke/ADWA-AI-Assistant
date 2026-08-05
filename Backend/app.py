b7af88f57fd4f06b919f3c1f9016a3bbbf8e9f64
Mebrie Awoke <mebrieawoke941@gmail.com>
2026-08-05 23:48:18 +0300

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -8,7 +8,7 @@ from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
 from src.prompt import *
 import os   
-
+  
 load_dotenv()
 
 GROQ_API_KEY = os.environ.get('GROQ_API_KEY')
